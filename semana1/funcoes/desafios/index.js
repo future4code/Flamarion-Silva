255a881894a998886c047306a1717e907717264a
Gazua300 <franfidinena@gmail.com>
2021-06-17 22:02:57 -0300

    Arquivo index.js modificado

--- semana1/funcoes/desafios/index.js
+++ semana1/funcoes/desafios/index.js
@@ -12,11 +12,12 @@ soma(2, 5)
 
 function teoPitagoras(cat1, cat2){
     let quadradoDaHipo = (cat1 * cat1) + (cat2 * cat2)
+    let hipo = quadradoDaHipo ** (1/2)    
     
-    return quadradoDaHipo
+    return hipo 
 }
 
-console.log(teoPitagoras(2, 2))
-console.log('O resultado em questão é o quadrado da hipotenusa, como não se pode utilizar o que ainda não foi ensinado, não encontrei meios para achar a raiz do resultado')
+console.log(teoPitagoras(3, 4))
+
 
 //O desafio de hoje foi espetacular. Queria agradecer a Labenu que está sendo uma Mãe Sabedoria pra meus estudos em javascript. Valeu!!!
