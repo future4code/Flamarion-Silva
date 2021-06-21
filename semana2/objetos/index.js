040a70910c769d6450522cad338fdddb59ef5e72
Gazua300 <franfidinena@gmail.com>
2021-06-21 17:07:15 -0300

    Arquivos index adicionado

---
+++ semana2/objetos/index.js
@@ -0,0 +1,42 @@
+const filme = {
+	nome: "Auto da Compadecida", 
+	ano: 2000, 
+	elenco: [
+		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+		"Virginia Cavendish"
+		], 
+	transmissoesHoje: [
+		{canal: "Telecine", horario: "21h"}, 
+		{canal: "Canal Brasil", horario: "19h"}, 
+		{canal: "Globo", horario: "14h"}
+		]
+}
+
+console.log(filme.elenco[0])
+console.log(filme.elenco[filme.elenco.length - 1])
+console.log(filme.transmissoesHoje[2])
+
+//Matheus Nachtergaele
+//Virginia Cavendish
+//Nada foi impresso no console
+
+const cachorro = {
+	nome: "Juca", 
+	idade: 3, 
+	raca: "SRD"
+}
+
+const gato = {...cachorro, nome: "Juba"}
+
+const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+console.log(cachorro)
+console.log(gato)
+console.log(tartaruga)
+
+/*
+{canal: "Globo", horario: "14h"}
+index.js:31 {nome: "Juca", idade: 3, raca: "SRD"}
+index.js:32 {nome: "Juba", idade: 3, raca: "SRD"}
+index.js:33 {nome: "Jubo", idade: 3, raca: "SRD"}
+*/
