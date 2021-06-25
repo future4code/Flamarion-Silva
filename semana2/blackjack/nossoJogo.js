459cce110dbcbf9838f7a87d19939d993d0b3326
Gazua300 <franfidinena@gmail.com>
2021-06-25 16:48:31 -0300

    Exercicio para terminar, pois procuro uma maneira de implementá-lo de forma completa

---
+++ semana2/blackjack/nossoJogo.js
@@ -0,0 +1,32 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * */
+const carta = comprarCarta(){
+         console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+         console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+
+    }
+    
+    * 
+ * 
+ * 
+ */
+ function blackjack(){
+     console.log('Boas vindas ao jogo Blackjack')
+     let msg = promot('Quer iniciar uma nova rodada')
+     
+     const catAS = {
+        texti: promopt()
+     }
+     
+     if(msg == cancel){
+         console.log('O jogo acabou!')
+         if(msg == true)
+         
+     }else
+     	
+     }
+ 
+ 
+ 
