76051a798944f7574671378c115485194a0b3eb0
Gazua300 <franfidinena@gmail.com>
2021-07-12 17:40:04 -0300

    Trabalho incompleto, surge não funcionou então estou colocando o link do git

---
+++ semana5/react/firtreact/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Teste básico para primeira aula de react</h1>
+     
+    </div>
+  );
+}
+
+export default App;
