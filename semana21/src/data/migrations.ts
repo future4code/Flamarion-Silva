6e3158a1ef4ac373f7f87be997d274d790fe97b5
Gazua300 <franfidinena@gmail.com>
2021-11-04 17:41:14 -0300

    entrega diaria

---
+++ semana21/src/data/migrations.ts
@@ -0,0 +1,13 @@
+create table pokemonGo(id varchar(255) primary key not null,
+pokedex_number varchar(255) not null name varchar(255) not null, generation vachar(255) null,
+evolution_stage varchar(255) not null, evolved varchar(255) not null,
+familyID varchar(255) not null, cross_gen varchar(255) not null,
+type1 varchar(255) not null, type2 varchar(255) not null,
+weather1 varchar(255) not null, weather2 varchar(255) not null,
+stat_total varchar(255) not null, atk varchar(255) not null,
+def varchar(255) not null, sta int not null, legendary vachar(255) not null,
+aquireable varchar(255) not null, spawns varchar(255) not null, regional varchar(255) not null,
+raidable varchar(255) not null, hatchable varchar(255) not null, shiny varchar(255) not null,
+nest varchar(255) not null, new varchar(255) not null, notgettable varchar(255) not null,
+future_evolve varchar(255) not null, cp40 varchar(255) not null,
+cp30 varchar(255) not null)
